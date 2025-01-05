6903a356e4bdaff7f4409489f071192fad3f311b
SundLyDev <167065053+Sullivankow@users.noreply.github.com>
2025-01-05 16:40:31 +0100

    Ajout attaquer un autre personnage en POO

--- Personnage.php
+++ Personnage.php
@@ -3,20 +3,31 @@
 class Personnage
 {
 
+
+    private int $winCombat = 0;
+
+
+    private const MIN_HEALTH = 10;
+
+    private const MAX_LEVEL = 100; //Je définis la constante pour le niveau max et stock 100 dans la variable MAX_LEVEL    
+    private const MIN_LEVEL = 1;
+    public const ATTACK_ACTION_KEY = 'attack';
+    public const DEFENSE_ACTION_KEY = 'defense';
+
+
+
+
     public function __construct(
         private string $name,
         private int $health,
         private int $attack,
         private int $defense,
-        private int $level = 1
+        private int $level = self::MIN_LEVEL,
     ) {}
 
 
 
-    public function reduceHealth(int $health)
-    {
-        $this->health -= $health;
-    }
+
 
 
 
@@ -27,9 +38,21 @@ class Personnage
     {
 
         if ($this->attack > $enemy->getDefense()) {
+
             $enemy->setHealth($enemy->getHealth() - ($this->attack - $enemy->getDefense())); //100 - (20 - 10), on récupère la santé de l'ennemi qui est de 100, - la différence entre l'attaque de notre personnage et la défense de l'ennemi
+            $this->increaseLevel(self::ATTACK_ACTION_KEY); //Notre personnage gagne, l'ennemi perd
+            $enemy->decreaseLevel();
+
+
+            $this->increaseWinCombat();
+            $enemy->decreaseWinCombat();
         } else {
             $this->setHealth($this->getHealth() - ($enemy->getDefense() - $this->attack)); // On récuère la santé de notre personnage - la différence entre la défense de l'ennemi et notre personnage
+            $this->decreaseLevel();
+            $enemy->increaseLevel();
+
+            $this->decreaseWinCombat(); //Notre personnage perd, l'ennemi gagne
+            $enemy->increaseWinCombat(self::DEFENSE_ACTION_KEY);
         }
     }
 
@@ -80,7 +103,7 @@ class Personnage
      */
     public function setHealth(int $health): self
     {
-        $this->health = $health < 0 ? 0 : $health;
+        $this->health = $health < self::MIN_HEALTH ? 0 : $health;
 
         return $this;
     }
@@ -119,5 +142,61 @@ class Personnage
         $this->defense = $defense;
 
         return $this;
+    }
+
+    public function getWinCombat(): int
+    {
+
+        return $this->winCombat;
+    }
+
+    public function increaseWinCombat(): self  //Fonction pour incrémenter le nombre de combat de 1 lorque l'ennemi n'a plus de santé
+    {
+
+        $this->winCombat++;
+
+        return $this;
+    }
+
+    public function decreaseWinCombat(): self //Fonction pour décrémenter le nombre de combat de 1 lorque notre personnage n'a plus de santé
+    {
+
+        $this->winCombat--;
+
+        return $this;
+
+        if ($this->winCombat == 0) {
+            $this->winCombat = 0;
+        }
+    }
+
+
+    //Fonction pour augmenter le niveau des personnages
+
+    private function increaseLevel(?string $type = null): void
+    {
+        if (self::ATTACK_ACTION_KEY === $type) {
+            $this->level += 2;
+        } elseif (self::DEFENSE_ACTION_KEY === $type) {
+
+            $this->level += 3;
+        } else {
+            $this->level += 1;
+        }
+
+        $this->level++;
+        if ($this->level > self::MAX_LEVEL) {  //Self sert à appeler des éléments statiques 
+            $this->level = self::MAX_LEVEL;
+        }
+    }
+
+    //Fonction pour baisser d'un level 
+    private function decreaseLevel(): void
+    {
+
+        $this->level--; //Si le level est inférieur à la constante 1 alors il est égal à 1
+        if ($this->level < self::MIN_LEVEL) {
+            $this->level = self::MIN_LEVEL;
+        }
     }
 }
--- index.php
+++ index.php
@@ -5,18 +5,28 @@
 
 require_once "Personnage.php";
 
-$merlin = new Personnage('Merlin', 100, 50, 80, 100);
+$merlin = new Personnage('Merlin', 100, 5, 80,);
 $arthur = new Personnage('Arthur', 100, 80, 10, 100);
 
 // $merlin->attack('Arthur');
 
 
-echo $merlin->getLevel() . "\r\n";
+echo $merlin->getHealth() . "\r\n";
 echo $arthur->getHealth() . "\n\r";
 
 echo "Merlin attaque Arthur" . "\r\n";
 $merlin->attack($arthur);
 
 
-echo $merlin->getLevel() . "\r\n";
+echo $merlin->getHealth() . "\r\n";
 echo $arthur->getHealth() . "\n\r";
+
+echo  "\n\r";
+
+echo $merlin->getLevel() . "\r\n";
+echo $arthur->getLevel() . "\n\r";
+
+echo  "\n\r";
+
+echo $merlin->getWinCombat() . "\r\n";
+echo $arthur->getWinCombat() . "\n\r";
